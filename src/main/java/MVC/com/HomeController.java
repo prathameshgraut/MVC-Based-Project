4f57c1b25a7b02624c045cd3397611d41ddaf8ba
Prathamesh Raut. <prathamesh1687@gmail.com>
2026-09-22 20:09:05 +0530

    Add UI

--- src/main/java/MVC/com/HomeController.java
+++ src/main/java/MVC/com/HomeController.java
@@ -2,16 +2,38 @@ package MVC.com;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 	
-	@GetMapping("/home")
-	public String HomePage() {
+	@GetMapping("/Login")
+	public String doGet() {
+		return "Login";
+	}
 
-	    System.out.println("Hello");
-
-	    return "Home";
+	@PostMapping("/Login")
+	public String doPost(@RequestParam String Username,@RequestParam String Password) {
+		if(Username.equals("Admin") && Password.equals("12345")) {
+			return "Dashboard";
+		}
+		return "Login";
+	}
+	
+	@GetMapping("/Logout")
+	public String doGetlogOut() {
+		return "Login";
+	}
+	
+	@GetMapping("/help")
+	public String doGetHelp() {
+		return "help";
+	}
+	
+	@GetMapping("/setting")
+	public String doGetSetting() {
+	    return "setting";
 	}
 	
 }
