6c8092f109b8cd33f5a3b1d62a8d898f3e3d2257
Prathamesh Raut. <prathamesh1687@gmail.com>
2026-09-22 15:22:58 +0530

    mvc

---
+++ src/main/java/MVC/com/MvcBasedProjectApplication.java
@@ -0,0 +1,13 @@
+package MVC.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcBasedProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcBasedProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MVC/com/MvcBasedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package MVC.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcBasedProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
